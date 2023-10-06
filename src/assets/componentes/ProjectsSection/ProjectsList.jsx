da4e7b43dd5b9801ffff8027d226c435a0d8e98e
Felipe Amorim <felipeamorim.go@gmail.com>
2023-10-06 03:55:29 -0300

    Troca de componentes

--- src/assets/componentes/ProjectsSection/ProjectsList.jsx
+++ src/assets/componentes/ProjectsSection/ProjectsList.jsx
@@ -8,32 +8,32 @@ const ProjectsList = () => {
         title="DESIGN PORTFOLIO"
         habilidades="HTML CSS"
         alt="DESIGN PORTFOLIO"
-        src="./src/assets/img/thumbnail-project-1-large.webp" />
+        src="./images/thumbnail-project-1-large.webp" />
       <ProjectItem
         title="E-LEARNING LANDING PAGE"
         habilidades="HTML CSS"
         alt="E-LEARNING LANDING PAGE"
-        src="./src/assets/img/thumbnail-project-2-large.webp" />
+        src="./images/thumbnail-project-2-large.webp" />
       <ProjectItem
         title="TODO WEB APP"
         habilidades="HTML CSS JAVASCRIPT"
         alt="TODO WEB APP"
-        src="./src/assets/img/thumbnail-project-3-large.webp" />
+        src="./images/thumbnail-project-3-large.webp" />
       <ProjectItem
         title="ENTERTAINMENT WEB APP"
         habilidades="HTML CSS JAVASCRIPT"
         alt="ENTERTAINMENT WEB APP"
-        src="./src/assets/img/thumbnail-project-4-large.webp" />
+        src="./images/thumbnail-project-4-large.webp" />
       <ProjectItem
         title="MEMORY GAME"
         habilidades="HTML CSS JAVASCRIPT"
         alt="MEMORY GAME"
-        src="./src/assets/img/thumbnail-project-5-large.webp" />
+        src="./images/thumbnail-project-5-large.webp" />
       <ProjectItem
         title="ART GALLERY SHOWCASE"
         habilidades="HTML CSS JAVASCRIPT"
         alt="ART GALLERY SHOWCASE"
-        src="./src/assets/img/thumbnail-project-6-large.webp" />
+        src="./images/thumbnail-project-6-large.webp" />
     </ul>
   )
 }
